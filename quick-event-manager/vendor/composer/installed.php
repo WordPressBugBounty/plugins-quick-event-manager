4c975b59a4227540433122167632009e10a586a1
Sean Murphy <sean@iamseanmurphy.com>
2024-09-24 17:21:43 +0000

    9.12

---
+++ quick-event-manager/vendor/composer/installed.php
@@ -0,0 +1,56 @@
+<?php return array(
+    'root' => array(
+        'name' => 'fullworks/quick-event-manager',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'c0c34680a61436cc24a7347fd6f4015b7165bc0d',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'composer/installers' => array(
+            'pretty_version' => 'v1.0.12',
+            'version' => '1.0.12.0',
+            'reference' => '4127333b03e8b4c08d081958548aae5419d1a2fa',
+            'type' => 'composer-installer',
+            'install_path' => __DIR__ . '/./installers',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'freemius/wordpress-sdk' => array(
+            'pretty_version' => '2.8.1',
+            'version' => '2.8.1.0',
+            'reference' => 'cc31279d088909135b6b2d8b4f2696312f5731fc',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../freemius/wordpress-sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'fullworks/quick-event-manager' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'c0c34680a61436cc24a7347fd6f4015b7165bc0d',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'gamajo/template-loader' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '05057216f60baebfc4939e1a2b9aae6e89d25c91',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../gamajo/template-loader',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'shama/baton' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+    ),
+);
